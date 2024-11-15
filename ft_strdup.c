d15c0158699519c8d04e8a6e39a91b6e08b51855
David Roque <d.n.roque@gmail.com>
2024-11-15 17:08:03 +0000

    patched valgrind/read errors

--- ft_strdup.c
+++ ft_strdup.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: david-fe <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: david-fe <david-fe@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/20 18:31:32 by david-fe          #+#    #+#             */
-/*   Updated: 2024/08/21 10:07:44 by david-fe         ###   ########.fr       */
+/*   Created: 2024/11/13 13:07:28 by david-fe          #+#    #+#             */
+/*   Updated: 2024/11/13 13:07:31 by david-fe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,15 @@
 char	*ft_strdup(const char *src)
 {
 	char	*ptrdup;
-	int		slen;
-	int		i;
+	size_t	slen;
+	size_t	i;
 
 	slen = ft_strlen((char *)src);
-	ptrdup = (char *) malloc(sizeof(char) * (slen + 1));
-	i = 0;
-	if (ptrdup == NULL)
+	ptrdup = malloc((slen + 1) * sizeof(char));
+	if (!ptrdup)
 		return (NULL);
-	while (i <= slen)
+	i = 0;
+	while (i < slen)
 	{
 		ptrdup[i] = src[i];
 		i++;
@@ -31,10 +31,11 @@ char	*ft_strdup(const char *src)
 	ptrdup[i] = '\0';
 	return (ptrdup);
 }
-/*
+
 int	main(void)
 {
 	char	*str = "copy me!";
-
-	printf("%s", ft_strdup(str));
-}*/
+	char	*new_str = ft_strdup(str); 
+	printf("%s\n", new_str);
+	free(new_str);
+}
